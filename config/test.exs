3f8f6ff6e7ca3ec5db26abe98f99ad12bb8d4e60
Michal Sewczyk <michal.szewczyk@appliscale.io>
2022-11-18 11:00:04 +0100

    Hide ecto logs

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,7 @@ config :ecto_discriminator, EctoDiscriminator.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  log: false
 
 config :ecto_discriminator, ecto_repos: [EctoDiscriminator.Repo]
