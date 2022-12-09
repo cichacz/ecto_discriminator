47b94746e7143d8429c7735827a99db152e9e77e
Michal Sewczyk <michal.szewczyk@appliscale.io>
2022-12-09 14:59:38 +0100

    Release 0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoDiscriminator.MixProject do
   def project do
     [
       app: :ecto_discriminator,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
