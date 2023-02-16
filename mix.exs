af71c74ba6198bd47139feedcd533f4fbe151f82
Michal Sewczyk <michal.szewczyk@appliscale.io>
2023-02-16 20:59:03 +0100

    Version 0.3.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EctoDiscriminator.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/cichacz/ecto_discriminator"
-  @version "0.3.1"
+  @version "0.3.2"
 
   def project do
     [
